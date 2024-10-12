d674a9a3b127f611ad4afad1863cd5dfe9a91f99
Swayam Kaushal <101446225+rajkaushal03@users.noreply.github.com>
2024-10-12 21:50:51 +0530

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,28 @@
-
+import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
-  
   return (
     <>
-      <HomePage/>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        {/* <Route
+          path="/explore"
+          element={authUser ? <ExplorePage /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/likes"
+          element={authUser ? <LikesPage /> : <Navigate to={"/login"} />}
+        /> */}
+      </Routes>
+      <Toaster />
+      {/* <HomePage/> */}
+      <SignupPage />
     </>
   );
 };
